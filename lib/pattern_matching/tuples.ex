b8300f7d0436c8fa420ba4a5f712c683f3742472
Dhony Silva <dhony.web@gmail.com>
2024-12-06 18:40:03 -0300

    Solving the day_from_date function

--- lib/pattern_matching/tuples.ex
+++ lib/pattern_matching/tuples.ex
@@ -11,20 +11,17 @@ defmodule PatternMatching.Tuples do
   Fix or complete the code to make the tests pass.
   """
 
-  def day_from_date(_erl_date) do
-
+  def day_from_date({_year, _month, day}) do
+    day
   end
 
   def has_three_elements?(_tuple) do
-
   end
 
   def major_us_holiday(_erl_date) do
-
   end
 
   def greet_user(_tuple), do: nil
 
   def add_to_result(_tuple), do: nil
-
 end
