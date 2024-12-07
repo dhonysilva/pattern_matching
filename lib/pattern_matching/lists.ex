3504aae934f1f8f2f5c3b0f4f88306ad7694a709
Dhony Silva <dhony.web@gmail.com>
2024-12-07 11:50:19 -0300

    Solving the is_empty? function

--- lib/pattern_matching/lists.ex
+++ lib/pattern_matching/lists.ex
@@ -11,32 +11,29 @@ defmodule PatternMatching.Lists do
   Fix or complete the code to make the tests pass.
   """
 
-  def is_empty?(_list) do
+  def is_empty?([]) do
+    true
+  end
 
+  def is_empty?(_list) do
+    false
   end
 
   def has_1_item?(_list) do
-
   end
 
   def at_least_one?(_list) do
-
   end
 
   def return_first_item(_list) do
-
   end
 
   def starts_with_1?(_list) do
-
   end
 
   def sum_pair(_list) do
-
   end
 
   def sum_first_2(_list) do
-
   end
-
 end
